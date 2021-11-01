05b0f9085c472db86236ab20cf2ca7f69cc8637d
mostrovski <ostrovskiime@gmail.com>
2021-11-01 17:23:40 +0100

    Add linter

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,14 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const fetch = require('node-fetch');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
 
@@ -18,11 +16,11 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 app.get('/apod', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
+            .then(res => res.json());
+        res.send({ image });
     } catch (err) {
         console.log('error:', err);
     }
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
