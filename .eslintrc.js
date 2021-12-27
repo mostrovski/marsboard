1c157786c7bf8a287b3980b5804b05a650ab26fd
mostrovski <ostrovskiime@gmail.com>
2021-12-27 22:22:33 +0100

    Add Immutable JS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
     'env': {
         'browser': true,
-        'commonjs': true,
         'es2021': true
     },
     'extends': 'eslint:recommended',
     'parserOptions': {
-        'ecmaVersion': 13
+        'ecmaVersion': 13,
+        'sourceType': 'module',
     },
     'rules': {
         'indent': [
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const fetch = require('node-fetch');
-const path = require('path');
+import dotenv from 'dotenv';
+import express from 'express';
+import fetch from 'node-fetch';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
+
+const API_BASE = process.env.API_BASE;
+const API_KEY = process.env.API_KEY;
+
+const buildRequestUri = (resource) => {
+    return `${API_BASE}/${resource}?api_key=${API_KEY}`;
+};
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/immutable', express.static(path.join(__dirname, '../../node_modules/immutable/dist')));
 
-// your API calls
-
-// example API call
-app.get('/apod', async (req, res) => {
+app.get('/manifests/:rover', async (req, res) => {
+    let rover = req.params.rover;
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        let manifest = await fetch(buildRequestUri(`manifests/${rover}`))
             .then(res => res.json());
-        res.send({ image });
+        res.send(manifest);
     } catch (err) {
         console.log('error:', err);
     }
