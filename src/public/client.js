a45d34e6a9bdf99a6ed790f31b3e1a50d97fca8e
mostrovski <ostrovskiime@gmail.com>
2021-12-28 23:45:58 +0100

    Start styling

--- src/public/client.js
+++ src/public/client.js
@@ -1,4 +1,4 @@
-import { fromJS } from './modules/immutable/dist/immutable.es.js'
+import { fromJS } from './modules/immutable/dist/immutable.es.js';
 
 let store = fromJS({
     rovers: {
@@ -33,24 +33,6 @@ const updateStore = (state, newState) => {
     render(root, store);
 };
 
-window.addEventListener('load', () => {
-    render(root, store);
-});
-
-// ------------------------------------------------------  COMPONENTS
-
-const RoverInfo = (name) => {
-    let data = store.getIn(['rovers', name]);
-
-    if (data.size === 0) {
-        getManifest(name);
-    }
-
-    return `
-        <h3>${data.get('name') ?? ''}</h3>
-    `;
-};
-
 // ------------------------------------------------------  API CALLS
 
 const getManifest = (rover) => {
@@ -64,3 +46,85 @@ const getManifest = (rover) => {
 
     return rover;
 };
+
+// ------------------------------------------------------  COMPONENTS
+
+const RoverInfo = (name) => {
+    let data = store.getIn(['rovers', name]);
+
+    if (data.size === 0) {
+        getManifest(name);
+    }
+
+    return `
+        <table class="table-auto border-collapse">
+            <thead></thead>
+            <tbody>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Rover</td>
+                    <td class="border-b p-4 pr-md-8">
+                        ${data.get('name') ?? PingingPoint('sky')}
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Launch date</td>
+                    <td class="border-b p-4 pr-md-8">
+                        ${data.get('launch_date') ?? PingingPoint('sky')}
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Landing date</td>
+                    <td class="border-b p-4 pr-md-8">
+                        ${data.get('landing_date') ?? PingingPoint()}
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Status</td>
+                    <td class="border-b p-4 pr-md-8 ${data.get('status') === 'active' ? 'text-teal-500' : 'text-stone-500'}">
+                        ${data.get('status') ?? PingingPoint('teal')}
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Most recent photos</td>
+                    <td class="border-b p-4 pr-md-8">
+                        ${data.get('max_date') ?? PingingPoint('teal')}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    `;
+};
+
+const PingingPoint = (color = 'default') => {
+    const background = {
+        default: {
+            light: 'bg-red-300',
+            dark: 'bg-red-400',
+        },
+        sky: {
+            light: 'bg-sky-300',
+            dark: 'bg-sky-400',
+        },
+        stone: {
+            light: 'bg-stone-300',
+            dark: 'bg-stone-400',
+        },
+        teal: {
+            light: 'bg-teal-300',
+            dark: 'bg-teal-400',
+        },
+    };
+
+    return `
+        <span class="absolute inline-flex h-3 w-3 top-5">
+            <span class="absolute inline-flex rounded-full h-full w-full animate-ping opacity-75 ${background[color]?.light ?? background.default.light}"></span>
+            <span class="relative inline-flex rounded-full h-3 w-3 ${background[color]?.dark ?? background.default.dark}"></span>
+        </span>
+    `;
+};
+
+// ------------------------------------------------------  INIT
+
+window.addEventListener('load', () => {
+    render(root, store);
+});
