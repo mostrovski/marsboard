56b3858634935a0d257901c294c4b40e4290a668
mostrovski <ostrovskiime@gmail.com>
2022-01-22 20:21:25 +0100

    Render header

--- src/public/client.js
+++ src/public/client.js
@@ -14,23 +14,31 @@ let store = fromJS({
     currentRover: 'curiosity',
 });
 
-const root = document.getElementById('root');
+const $root = document.getElementById('root');
 
 const App = (state) => {
     let name = state.get('currentRover');
 
     return `
-        ${RoverInfo(name)}
+        ${Header(name)}
+        <main class="container mx-auto px-8 py-4">
+            ${RoverInfo(name)}
+        </main>
     `;
 };
 
 const render = async (root, state) => {
     root.innerHTML = App(state);
+    document.dispatchEvent(new CustomEvent('rendered'));
 };
 
 const updateStore = (state, newState) => {
     store = state.mergeDeep(newState);
-    render(root, store);
+    render($root, store);
+};
+
+const capitalize = (word) => {
+    return word[0].toUpperCase() + word.substr(1);
 };
 
 // ------------------------------------------------------  API CALLS
@@ -49,49 +57,42 @@ const getManifest = (rover) => {
 
 // ------------------------------------------------------  COMPONENTS
 
-const RoverInfo = (name) => {
-    let data = store.getIn(['rovers', name]);
-
-    if (data.size === 0) {
-        getManifest(name);
-    }
+const RoverLink = (name, current) => {
+    let style = (name === current) ? 'text-gray-700 font-semibold' : 'underline underline-offset-4 decoration-red-400';
 
     return `
-        <table class="table-auto border-collapse">
-            <thead></thead>
-            <tbody>
-                <tr class="relative">
-                    <td class="border-b p-4 pl-md-8 font-semibold">Rover</td>
-                    <td class="border-b p-4 pr-md-8">
-                        ${data.get('name') ?? PingingPoint('sky')}
-                    </td>
-                </tr>
-                <tr class="relative">
-                    <td class="border-b p-4 pl-md-8 font-semibold">Launch date</td>
-                    <td class="border-b p-4 pr-md-8">
-                        ${data.get('launch_date') ?? PingingPoint('sky')}
-                    </td>
-                </tr>
-                <tr class="relative">
-                    <td class="border-b p-4 pl-md-8 font-semibold">Landing date</td>
-                    <td class="border-b p-4 pr-md-8">
-                        ${data.get('landing_date') ?? PingingPoint()}
-                    </td>
-                </tr>
-                <tr class="relative">
-                    <td class="border-b p-4 pl-md-8 font-semibold">Status</td>
-                    <td class="border-b p-4 pr-md-8 ${data.get('status') === 'active' ? 'text-teal-500' : 'text-stone-500'}">
-                        ${data.get('status') ?? PingingPoint('teal')}
-                    </td>
-                </tr>
-                <tr class="relative">
-                    <td class="border-b p-4 pl-md-8 font-semibold">Most recent photos</td>
-                    <td class="border-b p-4 pr-md-8">
-                        ${data.get('max_date') ?? PingingPoint('teal')}
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <a href="#" class="rover-link ${style}" data-rover="${name}">
+            ${capitalize(name)}
+        </a>
+    `;
+};
+
+const Header = (currentRover) => {
+    return `
+        <header class="h-36 md:h-20 pt-1 sm:pt-0 bg-gray-50 shadow-2xl sticky top-0 px-8">
+            <div class="md:flex space-y-4 md:space-y-0 items-center justify-between h-full container">
+                <div class="flex-1">
+                    <h1 class="text-center md:text-left text-2xl uppercase tracking-wider text-gray-700 font-bold">
+                        Marsboard
+                    </h1>
+                </div>
+                <div class="flex-1 text-center">
+                    <div class="space-x-2">
+                        ${RoverLink('curiosity', currentRover)}
+                        ${RoverLink('opportunity', currentRover)}
+                        ${RoverLink('spirit', currentRover)}
+                    </div>
+                </div>
+                <div class="flex-1 text-center md:text-right">
+                    <a href="https://open.spotify.com/album/6GjlSsOUUl1VlspqO2Vw9U"
+                       target="_blank"
+                       class="underline underline-offset-4 decoration-sky-500"
+                    >
+                        Stay on Earth
+                    </a>
+                </div>
+            </div>
+        </header>
     `;
 };
 
@@ -123,8 +124,65 @@ const PingingPoint = (color = 'default') => {
     `;
 };
 
+const RoverInfo = (name) => {
+    let data = store.getIn(['rovers', name]);
+
+    if (data.size === 0) {
+        getManifest(name);
+    }
+
+    return `
+        <table class="table-auto border-collapse">
+            <thead></thead>
+            <tbody>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Rover</td>
+                    <td class="border-b p-4 pr-md-8">
+                        ${data.get('name') ?? PingingPoint('sky')}
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Launch date</td>
+                    <td class="border-b p-4 pr-md-8">
+                        ${data.get('launch_date') ?? PingingPoint('sky')}
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Landing date</td>
+                    <td class="border-b p-4 pr-md-8">
+                        ${data.get('landing_date') ?? PingingPoint()}
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Mission Status</td>
+                    <td class="border-b p-4 pr-md-8 ${data.get('status') === 'active' ? 'text-teal-500' : 'text-stone-500'}">
+                        ${data.get('status') ?? PingingPoint('teal')}
+                    </td>
+                </tr>
+                <tr class="relative">
+                    <td class="border-b p-4 pl-md-8 font-semibold">Most recent photos</td>
+                    <td class="border-b p-4 pr-md-8">
+                        ${data.get('max_date') ?? PingingPoint('teal')}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    `;
+};
+
 // ------------------------------------------------------  INIT
 
+document.addEventListener('rendered', () => {
+    document.querySelectorAll('.rover-link').forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            updateStore(store, {
+                currentRover: event.target.dataset.rover,
+            });
+        });
+    });
+});
+
 window.addEventListener('load', () => {
-    render(root, store);
+    render($root, store);
 });
